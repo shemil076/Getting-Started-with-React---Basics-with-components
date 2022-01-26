50ed946c9304bcacecc6b07ccbfba3787fbe5ad4
Pramuditha Karunarathna <shemil076@gmail.com>
2022-01-26 22:43:29 +0530

    Implementing logout to remove user Authentication Token

--- src/components/todo/AuthenticationService.js
+++ src/components/todo/AuthenticationService.js
@@ -2,6 +2,10 @@ class AuthenticationService{
     registerSuccessfulLogin(username, password){
         sessionStorage.setItem('username', username);
     } 
+
+    logOutSuccessfulLogin(){
+        sessionStorage.removeItem('username');
+    }
 }
 
 export default new AuthenticationService()
