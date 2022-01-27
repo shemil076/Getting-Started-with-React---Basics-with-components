cbe8eee40b6465e5951a42c34a7dd79645be5fb5
Pramuditha Karunarathna <shemil076@gmail.com>
2022-01-27 13:08:12 +0530

    Connecting React with Restful API - 1 - Preparing Welcome Page

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -3,18 +3,35 @@ import {Link} from 'react-router-dom';
 
 
 class WelcomeComponent extends Component {
+
+    constructor(props) {
+        super(props);
+
+        this.retrieveWelcomeMessage = this.retrieveWelcomeMessage.bind(this);
+    }
     render() {
         return (
             <div>
                 <h1>welcome</h1>
 
                 <div className="container">
-                    Welcome {this.props.params.name}. You can manage your todos <Link to="/todos">here</Link>
+                    Welcome {this.props.params.name}. 
+                    You can manage your todos <Link to="/todos">here</Link>
                 </div>
 
+                <div className="container">
+                   Click here to get a customised welcome message
+                    <button onClick={this.retrieveWelcomeMessage} 
+                        className="btn btn-success">Get Welcome Message
+                    </button>
+                </div>
 
             </div>
         )
+    }
+
+    retrieveWelcomeMessage(){
+
     }
 }
 
