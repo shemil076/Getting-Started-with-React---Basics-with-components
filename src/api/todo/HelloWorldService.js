56ac7f7c8b55084a2bf16d12f6c2f3da5d47c904
Pramuditha Karunarathna <shemil076@gmail.com>
2022-01-28 14:55:26 +0530

    Enhancing React Welcome Data Service to use Basic Auth

--- src/api/todo/HelloWorldService.js
+++ src/api/todo/HelloWorldService.js
@@ -11,7 +11,20 @@ class HelloWorldService{
      }
 
      executeHelloWorlPathVariableService(name){
-      return axios.get(`http://localhost:8080/hello-world/path-variable/${name}`)
+
+      let username = 'in28minutes'
+      let password = 'dummy'
+
+      let basicAuthHeader = 'Basic ' + window.btoa(username + ':' + password)
+
+      return axios.get(`http://localhost:8080/hello-world/path-variable/${name}`,
+      {
+         headers: {
+            authorization: basicAuthHeader
+         }
+      }
+
+      );
    }
 }
 
--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -62,7 +62,18 @@ class WelcomeComponent extends Component {
     }
 
     handleError(error){
-        this.setState({errorMessage : error.response.data.message})
+
+        let errorMessage = '';
+
+        if(error.message){
+            errorMessage += error.message
+        }
+
+        if(error.response && error.response.data){
+            errorMessage += error.response.data.message
+        }
+
+        this.setState({errorMessage : errorMessage})
     }
 }
 
