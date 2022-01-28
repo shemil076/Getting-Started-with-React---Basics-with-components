e889642808c567d2a522abeebbd0b2a3fc071371
Pramuditha Karunarathna <shemil076@gmail.com>
2022-01-28 10:15:59 +0530

    Implement Form Validation with Formik

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import moment from 'moment';
-import { Form, Formik, Field } from 'formik';
+import { Form, Formik, Field, ErrorMessage} from 'formik';
 
 class TodoComponent extends Component{
     constructor(props) {
@@ -11,7 +11,25 @@ class TodoComponent extends Component{
             targetDate : moment(new Date()).format('YYYY-MM-DD')
         }
         this.onSubmit = this.onSubmit.bind(this);
+        this.validate = this.validate.bind(this);
     }
+
+    validate(values) {
+        let errors = {}
+
+        if(!values.description){
+            errors.description = 'Enter a description';
+        }else if(values.description.length < 5){
+            errors.description = 'Enter atleast 5 characters in description';
+        }
+
+       if(!moment(values.targetDate).isValid()) {
+           errors.targetDate = 'Please enter a valid date';
+       }
+
+        return errors;
+    }
+
 
     onSubmit(values) {
         
@@ -27,18 +45,27 @@ class TodoComponent extends Component{
                     <Formik 
                         initialValues={{ description, targetDate }}
                         onSubmit={this.onSubmit}
+                        validateOnChange={true}
+                        validateOnBlur={true}
+                        validate={this.validate}
                     >
                         {
                             (props) =>(
                                 <Form>
+                                    <ErrorMessage name="description" component="div"
+                                        className="alert alert-warning" />
+
+                                    <ErrorMessage name="targetDate" component="div"
+                                        className="alert alert-warning" />
+
                                     <fieldset className="form-group">
                                         <label>Description</label>
-                                        <Field className="form-control" type="text" name="Description"/>
+                                        <Field className="form-control" type="text" name="description"/>
                                     </fieldset>
 
                                     <fieldset className="form-group">
                                         <label>TargetDate</label>
-                                        <Field className="form-control" type="date" name="TargetDate"/>
+                                        <Field className="form-control" type="date" name="targetDate"/>
                                     </fieldset>
                                     <button type="submit" className="btn btn-success">Save</button>
                                 </Form>
