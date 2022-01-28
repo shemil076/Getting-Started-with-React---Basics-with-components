24ad9350b0aeb9096ddace7c58f23d025f56292a
Pramuditha Karunarathna <shemil076@gmail.com>
2022-01-28 15:22:46 +0530

    Creating React Axios Intercepter to Add basic Auth Header

--- src/components/todo/AuthenticationService.js
+++ src/components/todo/AuthenticationService.js
@@ -1,6 +1,8 @@
+import axios from "axios";
 class AuthenticationService{
     registerSuccessfulLogin(username, password){
         sessionStorage.setItem('authenticatedUser', username);
+        this.setupAxiosInterceptor()
     } 
 
     logOutSuccessfulLogin(){
@@ -17,6 +19,24 @@ class AuthenticationService{
         let user = sessionStorage.getItem('authenticatedUser')
         if(user ==null) return '';
         return user;
+    }
+
+
+
+    setupAxiosInterceptor(){
+        let username = 'in28minutes'
+        let password = 'dummy'
+  
+        let basicAuthHeader = 'Basic ' + window.btoa(username + ':' + password)
+
+        axios.interceptors.request.use(
+            (config) => {
+                if(this.isUserLoggedIn){
+                    config.headers.authorization = basicAuthHeader
+                }
+                return config
+            }
+        )
     }
 }
 
