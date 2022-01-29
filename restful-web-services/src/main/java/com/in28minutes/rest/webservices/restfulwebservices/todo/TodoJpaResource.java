d947046124b375c8a6df8868536f0c1685acef47
Pramuditha Karunarathna <shemil076@gmail.com>
2022-01-29 17:30:54 +0530

    Connecting POST, PUT and DELETE REST APIs to JPA Repository

--- restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoJpaResource.java
+++ restful-web-services/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/TodoJpaResource.java
@@ -37,22 +37,22 @@ public class TodoJpaResource {
 
     @DeleteMapping("/jpa/users/{username}/todos/{id}")
     public ResponseEntity<Void> deleteTodo(@PathVariable String username, @PathVariable long id) {
-        Todo todo = todoService.deleteById(id);
-        if(todo != null) {
-            return ResponseEntity.noContent().build();
-        }
-        return ResponseEntity.notFound().build();
+        todoJpaRepository.deleteById(id);
+        return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/jpa/users/{username}/todos/{id}")
     public ResponseEntity<Todo> updateTodo(@PathVariable String username, @PathVariable long id, @RequestBody Todo todo){
-        Todo todoUpdated = todoService.save(todo);
+        todo.setUsername(username);
+        Todo todoUpdated = todoJpaRepository.save(todo);
         return new ResponseEntity<Todo>(todo, HttpStatus.OK);
     }
 
     @PostMapping("/jpa/users/{username}/todos")
-    public ResponseEntity<Void> updateTodo(@PathVariable String username, @RequestBody Todo todo){
-        Todo createdTodo = todoService.save(todo);
+    public ResponseEntity<Void> createdTodo(@PathVariable String username, @RequestBody Todo todo){
+
+        todo.setUsername(username);
+        Todo createdTodo = todoJpaRepository.save(todo);
 
         URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdTodo.getId()).toUri();
         return ResponseEntity.created(uri).build();
